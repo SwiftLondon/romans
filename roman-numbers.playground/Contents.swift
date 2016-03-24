aa31f39701b196b821df785f822ac72b77ae6676
Kovács Dániel Dávid <kicsikrumpli@kovacss-mbp.budapest.epam.com>
2016-03-24 12:47:30 +0100

    first draft roman to arabic

--- roman-numbers.playground/Contents.swift
+++ roman-numbers.playground/Contents.swift
@@ -2,8 +2,85 @@
 
 import UIKit
 
+enum RomanNumeralSymbol: Int {
+    case I = 1
+    case V = 5
+    case X = 10
+    case L = 50
+    case C = 100
+    case D = 500
+    case M = 1000
+    static func parse(char: Character) -> RomanNumeralSymbol? {
+        switch char {
+        case "I":
+            return I
+        case "V":
+            return V
+        case "X":
+            return X
+        case "L":
+            return L
+        case "C":
+            return C
+        case "D":
+            return D
+        case "M":
+            return M
+        default:
+            return nil
+        }
+    }
+}
+
+struct Token {
+    var symbol: RomanNumeralSymbol
+    var count: Int
+}
+
+func parseRomanNumeral(romanNumber: String) -> [Token] {
+    var tokens = [Token]()
+    romanNumber.characters.forEach({(char: Character) in
+        if let nextSymbol = RomanNumeralSymbol.parse(char) {
+            if tokens.count < 1 || tokens.last!.symbol != nextSymbol {
+                tokens.append(Token(symbol: nextSymbol, count: 1))
+            } else {
+                let oldToken = tokens.popLast()!
+                // validate count?
+                tokens.append(Token(symbol: nextSymbol, count: oldToken.count + 1));
+            }
+        } else {
+            // todo! handle invalid token
+        }
+    })
+    return tokens
+}
+
+func processRomanNumeralTokens(tokens: [Token]) -> Int {
+    var acc = 0
+    let tokenCount = tokens.count
+    for i in 0..<tokenCount {
+        let currentToken = tokens[i]
+        let currentTokenValue = currentToken.symbol.rawValue * currentToken.count
+        if i < tokenCount-1 && currentToken.symbol.rawValue < tokens[i+1].symbol.rawValue {
+            acc -= currentTokenValue
+        } else {
+            acc += currentTokenValue
+        }
+    }
+    return acc
+}
+
+processRomanNumeralTokens(parseRomanNumeral("II"))
+processRomanNumeralTokens(parseRomanNumeral("III"))
+processRomanNumeralTokens(parseRomanNumeral("IV"))
+processRomanNumeralTokens(parseRomanNumeral("V"))
+processRomanNumeralTokens(parseRomanNumeral("VI"))
+processRomanNumeralTokens(parseRomanNumeral("VIII"))
+processRomanNumeralTokens(parseRomanNumeral("MMXVI"))
+
+
 func romanToNum(romanNumber: String) -> Int {
-    return romanNumber.characters.count
+    return processRomanNumeralTokens(parseRomanNumeral(romanNumber))
 }
 
 func numToRoman(number: Int) -> String {
@@ -22,3 +99,12 @@ assert(numToRoman(romanToNum(III)) == III, "III -> 3 -> III")
 
 let IV = "IV"
 assert(romanToNum(IV) == 4, "IV is four")
+
+assert(romanToNum("II") == 2, "II is 2")
+assert(romanToNum("III") == 3, "III is 3")
+assert(romanToNum("IV") == 4, "IV is 4")
+assert(romanToNum("V") == 5, "V is 5")
+assert(romanToNum("VI") == 6, "VI is 6")
+assert(romanToNum("VIII") == 8, "VIII is 8")
+assert(romanToNum("MMXVI") == 2016, "MMXVI is 2016")
+
